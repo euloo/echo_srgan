f27e73654fc3a133ff0e11fce1bbcb17b122cd2a
Julia Komleva <komleva.j@gmail.com>
2021-03-30 17:37:00 +0500

    configs

---
+++ train_c1_b256.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --job-name=net_training            # Название задачи
+##SBATCH --error=my_numpy_task-%j.err        # Файл для вывода ошибок
+##SBATCH --output=my_numpy_task-%j.log       # Файл для вывода результатов
+#SBATCH -o my.stdout
+#SBATCH --time=1:00:00                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=8                # Выполнение расчёта на 2 ядрах CPU
+#SBATCH --gpus=1                            # Требуемое кол-во GPU
+python src/main.py --dataset_path=../../datasets/CAMUS --config=configs/ventricle_b256.json --use_wandb=True --wandb_run_name=b256_c1 --cuda=1
